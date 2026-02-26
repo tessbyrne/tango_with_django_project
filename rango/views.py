2e88cc720da7ebab9a6698752ec01c8a502fe91b
tessbyrne <3117688B@student.gla.ac.uk>
2026-02-26 14:57:56 +0000

    Chapter 4 excercise commits

--- rango/views.py
+++ rango/views.py
@@ -6,4 +6,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
+    context_message = {'boldmessage': 'This tutorial has been put together by Tess.'}
+    return render(request, 'rango/about.html', context=context_message)
