2f40c961669ae95490fe7e0b6d8908d0ead3323d
tessbyrne <3117688B@student.gla.ac.uk>
2026-02-26 13:05:16 +0000

    Hyperlink fixes - chapter 3

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!<br>/rango/about/About Page</a></a>")
+    return HttpResponse("Rango says hey there partner!<a href='/rango/about/'>About Page</a>")
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<br>/rango/Index</a>")
+    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
