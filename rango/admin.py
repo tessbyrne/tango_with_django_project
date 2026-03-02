58d80e9902f9618ad37014558db28e9a515030b2
tessbyrne <3117688B@student.gla.ac.uk>
2026-03-02 17:13:59 +0000

    Chapter 9 commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
@@ -9,3 +10,5 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
+
